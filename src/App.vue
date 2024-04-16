224abae9f1d68590340876e3d33cf420818fbc01
Filipe Conde <filipecondep@gmail.com>
2024-04-16 17:31:58 -0300

    sets background color for project.

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background: #c2c9b5;
 }
 </style>
