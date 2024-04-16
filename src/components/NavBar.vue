e9bacc52b5f69adcf11d39073d98b3e82afb1abc
Filipe Conde <filipecondep@gmail.com>
2024-04-16 14:57:52 -0300

    creates navigation bar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <div class="container">
+      <a href="#" class="previous-btn">
+        <img src="../assets/angle-left.svg" alt="" class="previous-btn
+        ">
+      </a>
+      <img src="../assets/online-shopping.png" alt="" class="logo" />
+      <a href="shopping-cart" class="shopping-cart">
+        <img src="../assets/shopping-cart.svg" alt="" class="shopping-cart__img">
+      </a>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+.logo {
+  width: 50px;
+  height: 50px;
+  padding: 0.5rem;
+}
+
+.shopping-cart__img {
+  height: 30px;
+  /* align-items: center; */
+}
+
+.previous-btn {
+  height: 20px;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+}
+
+section {
+  background-color: rgb(91, 148, 103);
+  box-shadow: 0px 2px 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
+  <NavBar />
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  </div> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    NavBar
   },
 });
 </script>
