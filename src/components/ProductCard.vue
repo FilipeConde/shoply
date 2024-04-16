c1336da6c630aed40c52aafe078e29dc106c3cb5
Filipe Conde <filipecondep@gmail.com>
2024-04-16 19:50:06 -0300

    Shows fixture data from products on screen.

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container" v-for="product in products" :key="product.name">
+    <div class="container__product-data">
+      <img src="" alt="" class="product-img product">
+      <div class="product-data product">
+        <h1 class="product-name">{{ product.name}}</h1>
+        <p class="product-description">{{ product.description }}</p>
+        <p class="product-price">R${{ product.price}}</p>
+      </div>
+    </div>
+    <div class="container__add-btn"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import productsJson from '@/fixtures/productList.json'
+import IProduct from '@/interfaces/IProduct'
+
+export default defineComponent({
+  data() {
+    return {
+      products: [] as IProduct[]
+    }
+  },
+  created() {
+    this.products = productsJson
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,19 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div> -->
+  <ProductCard />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ProductCard from '@/components/ProductCard.vue';
 import NavBar from '@/components/NavBar.vue'
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    NavBar
+    NavBar, ProductCard
   },
 });
 </script>
