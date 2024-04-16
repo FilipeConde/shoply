a5b37be09c4860097860739a9c3f7766ff07d18c
Filipe Conde <filipecondep@gmail.com>
2024-04-16 17:40:10 -0300

    Creates product interface.

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,8 @@
+type Category = ['Salgados', 'Doces', 'Bebidas', 'Pratos Quentes']
+
+export default interface IProduct {
+  name: string,
+  description: string
+  price: number,
+  category: Category
+}
